4caea54caffe50e731382f7ecb09db139d00b5c7
Walmir-Junior <juniorsouzafig@gmail.com>
2022-04-27 19:01:01 -0300

    Helany socorro

---
+++ modulo3/projeto-whatsLab/whats-lab/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="screen">
+      <div className="base-chat">
+        <div className="background-chat">
+          <div className="chat-area">
+            
+          </div>
+          <div className="mensage-area">
+            <div className="input-user">
+              
+            </div>
+            <div className="input-mensage">
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
