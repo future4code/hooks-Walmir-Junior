fd93ee1f84698ecae1644b56a4b0eb9f54c88712
Walmir-Junior <juniorsouzafig@gmail.com>
2022-06-19 17:28:16 -0300

    inicio API

--- modulo4/labex/src/Pages/Public/ApplicationForm/ApplicationForm.js
+++ modulo4/labex/src/Pages/Public/ApplicationForm/ApplicationForm.js
@@ -8,19 +8,19 @@ const ApplicationForm = () => {
         <>
           <button onClick={() => back(navigate)}>voltar</button> 
             <form>
-                <div>
                     <select><option selected disabled>Escolha uma viagem</option></select>
 
                     <input placeholder="nome" />
 
-                    <input placeholder="idade" />
+                    <input placeholder="idade" type="number" />
 
                     <input placeholder="Texto de candidatura" />
 
                     <input placeholder="Profissão" />
 
                     <select><option selected disabled>Selecione seu país</option></select>
-                </div>
+
+                    <button type="submit">Enviar</button>
             </form>
         </>
     )
--- modulo4/labex/src/Pages/Public/ListTrip/ListTrip.js
+++ modulo4/labex/src/Pages/Public/ListTrip/ListTrip.js
@@ -1,12 +1,33 @@
 import React from "react"
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 import { goToForm, back } from "../../../Coordinator/Coordinator"
-const ListTrip = () => {   
+import useRequestData from "../../../hooks/useRequestData"
+
+
+const ListTrip = () => {
     const navigate = useNavigate()
+
+    const trips = useRequestData("https://us-central1-missao-newton.cloudfunctions.net/futureX/darvas/trips")
+
+    const renderTrips = trips && trips.map((trip) => {
+        return (
+            <div key={trip.id}>
+                <div key={trip.id}>
+                    <p>{trip.name}</p>
+                    <p>{trip.description}</p>
+                    <p>{trip.planet}</p>
+                    <p>{trip.date}</p>
+                </div>
+                <hr />
+            </div>
+        )
+    })
     return (
         <>
             <button onClick={() => goToForm(navigate)}>Inscrever-se</button>
-            <button onClick={() => back(navigate)}>voltar</button> 
+            <button onClick={() => back(navigate)}>voltar</button>
+
+            {renderTrips}
         </>
     )
 }
